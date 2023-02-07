0e770e5e89b2e313a051588e1eedbe08f238f3d8
Paula Yagnycz <123567908+CodePaula@users.noreply.github.com>
2023-02-07 19:09:22 -0300

    Update tabula.read_pdf.py

--- tabula.read_pdf.py
+++ tabula.read_pdf.py
@@ -1,7 +1,7 @@
 import tabula
 from tabula import read_pdf
 
-df = tabula.read_pdf('nome_do_arquivo.pdf', pages = 5, lattice = True) 
+df = tabula.read_pdf('file_name.pdf', pages = 2, lattice = True) #The parameter "pages" is the page that you want to read.
 tabela = df[0]
 tabela = tabela.dropna(how = "all")
 tabela = tabela.dropna(how = "all", axis = 1)
