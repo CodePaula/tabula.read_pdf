b917f8418e5cd2e3edaca71b4b1ab041c3227526
Paula Yagnycz <123567908+CodePaula@users.noreply.github.com>
2023-02-07 18:04:32 -0300

    Add files via upload

---
+++ tabula.read_pdf.py
@@ -0,0 +1,11 @@
+import tabula
+from tabula import read_pdf
+
+df = tabula.read_pdf('MGLU_ER_3T20_POR.pdf', pages = 5, lattice = True)
+tabela = df[0]
+tabela = tabela.dropna(how = "all")
+tabela = tabela.dropna(how = "all", axis = 1)
+tabela.columns = tabela.iloc[0]
+tabela = tabela.drop(tabela.index[0])
+tabela = tabela.reset_index(drop = True)
+print(tabela)
