a94f1b0aca7d5f1ace28d296a3344d584974f1f7
Paula Yagnycz <123567908+CodePaula@users.noreply.github.com>
2023-02-07 19:37:14 -0300

    Update tabula.read_pdf.py

--- tabula.read_pdf.py
+++ tabula.read_pdf.py
@@ -3,7 +3,7 @@ from tabula import read_pdf
 
 df = tabula.read_pdf('file_name.pdf', pages = 2, lattice = True) #The parameter "pages" is the page that you want to read.
 tabela = df[0]
-tabela = tabela.dropna(how = "all")
+tabela = tabela.dropna(how = "all") #These methods are used to organize data in the file.
 tabela = tabela.dropna(how = "all", axis = 1)
 tabela.columns = tabela.iloc[0]
 tabela = tabela.drop(tabela.index[0])
